970d212b6f15f52af26951db7dd7b96872cb68ce
Maarten <maartenzam@gmail.com>
2023-03-29 13:45:08 +0200

    Base path and build

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-netlify';
-//import adapter from '@sveltejs/adapter-static';
+//import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		// Set the path where the app will live here (starting but not ending with a '/')
+		paths: {base: ''}
 	}
 };
 
